a0ac4cb84005f8c5859211e1a0a980244413c6bd
VladZGarcia <vladi.garcia@gmail.com>
2022-12-07 11:48:00 +0100

    Initial Commit

---
+++ Luffarschack/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Luffarschack
+//
+//  Created by Hugo Garcia on 2022-12-07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
